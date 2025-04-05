9b5191efa9117acb1f1791cc66a801872c76bcb4
Oussamabenazine <benazine.oussama@gmail.com>
2025-04-05 06:40:55 +0100

    Ekher update

--- Assets/scripts/sceneManager.cs
+++ Assets/scripts/sceneManager.cs
@@ -3,58 +3,107 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class sceneManager : MonoBehaviour
+public class SceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
-
+    // Reference to UI panels
     public GameObject Panel_selection;
     public GameObject Panel_intro;
+
+    // Dictionary to map scene names to their build indices
+    private readonly Dictionary<string, int> sceneIndices = new Dictionary<string, int>
+    {
+        { "Starting Menu", 0 },
+        { "Main Menu", 1 },
+        { "place on plane", 2 },
+        { "place on image", 3 },
+        { "Language Testing", 4 },
+        { "Quiz", 5 }, // Added for the new scene
+        { "test simulation", 6 },
+        { "simulation", 7 }
+    };
+
     void Start()
     {
-        
+        // Optionally, initialize any default state here
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // No updates needed in this case
     }
 
-    // Function to load scene with index 1
-    public void LoadSceneWithIndex1()
+    // Function to load a scene by name
+    public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(1);
+        if (sceneIndices.ContainsKey(sceneName))
+        {
+            SceneManager.LoadScene(sceneIndices[sceneName]);
+        }
+        else
+        {
+            Debug.LogError($"Scene '{sceneName}' not found in the build settings.");
+        }
     }
 
-    // Function to load scene with index 2
-    public void LoadSceneWithIndex2()
+    // Specific scene loading methods for convenience
+    public void LoadSceneIntro()
     {
-        SceneManager.LoadScene(2);
+        LoadScene("Starting Menu");
     }
-     public void LoadSceneIntro()
+
+    public void LoadSceneRack()
     {
-        SceneManager.LoadScene(0);
+        LoadScene("Main Menu");
     }
-      public void LoadSceneRack()
+
+    public void LoadScenePlaceOnPlane()
     {
-        SceneManager.LoadScene(3);
+        LoadScene("place on plane");
     }
-    public void quit()
+
+    public void LoadScenePlaceOnImage()
+    {
+        LoadScene("place on image");
+    }
+
+    public void LoadSceneLanguageTesting()
+    {
+        LoadScene("Language Testing");
+    }
+
+    public void LoadSceneQuiz()
+    {
+        LoadScene("Quiz"); // New method for the Quiz scene
+    }
+
+    public void LoadSceneTestSimulation()
+    {
+        LoadScene("test simulation");
+    }
+
+    public void LoadSceneSimulation()
+    {
+        LoadScene("simulation");
+    }
+
+    public void Quit()
     {
         Application.Quit();
     }
-    public void languageSelection()
-    {
-        //open scene language selection
 
+    public void LanguageSelection()
+    {
+        // Logic to open language selection scene
+        LoadScene("Language Testing"); // Assuming "Language Testing" is the language selection scene
     }
 
-    public void startscene()
+    public void StartScene()
     {
         Panel_intro.SetActive(false);
         Panel_selection.SetActive(true);
     }
-    public void back()
+
+    public void Back()
     {
         Panel_intro.SetActive(true);
         Panel_selection.SetActive(false);
